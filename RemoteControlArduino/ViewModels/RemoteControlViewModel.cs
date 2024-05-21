22df2851e1aaff128caebeba8561f37e9e47a687
Aleksey Zabrodin <rediskal50@gmail.com>
2024-05-21 21:26:55 +0300

    ref: add buttons for connecting to Ports

--- RemoteControlArduino/ViewModels/RemoteControlViewModel.cs
+++ RemoteControlArduino/ViewModels/RemoteControlViewModel.cs
@@ -1,19 +1,58 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.IO.Ports;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RemoteControlArduino.ViewModels
 {
-    public partial class RemoteControlViewModel
+    public partial class RemoteControlViewModel : ObservableObject
     {
+        [ObservableProperty]
+        public string[]? _portNamesList;
+
+        [ObservableProperty]
+        public string? _selectedPort;
+
+        [ObservableProperty]
+        public string? _connectButtonName = "Connect";
+
+
+
+
+
+
+
 
         [RelayCommand]
-        private void ButtonClick()
+        private void ConnectPort()
         {
+            var serialPort = new SerialPort();
 
+            try
+            {
+                serialPort.PortName = SelectedPort;
+                serialPort.Open();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Порт занят");
+            }
+        }
+
+        [RelayCommand]
+        private void UpdatePorts()
+        {
+            PortNamesList = SerialPort.GetPortNames();
+
+            if (PortNamesList.Length != 0)
+            {
+                SelectedPort = PortNamesList[0];
+            }
         }
 
     }

26076d8879f290456890306aac6b4bfe7033cfc2
Aleksey Zabrodin <rediskal50@gmail.com>
2024-05-21 14:53:38 +0300

    feat: create project

---
+++ RemoteControlArduino/ViewModels/RemoteControlViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteControlArduino.ViewModels
+{
+    public partial class RemoteControlViewModel
+    {
+
+        [RelayCommand]
+        private void ButtonClick()
+        {
+
+        }
+
+    }
+}

ca0f357a3149d49989a3216acb52dcc5186db8a6
ADANI\zabrodin <rediskal50@gmail.com>
2024-05-25 11:54:49 +0300

    ref: refactoring method connect

--- RemoteControlArduino/ViewModels/RemoteControlViewModel.cs
+++ RemoteControlArduino/ViewModels/RemoteControlViewModel.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 
 namespace RemoteControlArduino.ViewModels
 {
@@ -21,30 +22,76 @@ namespace RemoteControlArduino.ViewModels
         [ObservableProperty]
         public string? _connectButtonName = "Connect";
 
+        [ObservableProperty]
+        public bool? _portIsEnabled = true;
+
+        [ObservableProperty]
+        public SerialPort _serialPort = new SerialPort();
+
+        [ObservableProperty]
+        public string? _outputInfo;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(ConnectPortCommand))]
+        public bool _conectionButtonIsEnabled;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(UpdatePortsCommand))]
+        public bool _updateButtonIsEnabled = true;
+
+        [ObservableProperty]
+        public Brush _comboBoxColor = Brushes.Black;
+
+        [ObservableProperty]
+        public Brush _outputInfoColor = Brushes.Black;
+
+        [ObservableProperty]
+        public Brush _connectButtonColor = Brushes.YellowGreen;
 
 
 
-
-
-
-
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(ConectionButtonIsEnabled))]
         private void ConnectPort()
         {
-            var serialPort = new SerialPort();
+            if (ConnectButtonName == "Connect")
+            {
+                try
+                {
+                    SerialPort.PortName = SelectedPort;
+                    SerialPort.Open();
+                    PortIsEnabled = false;
 
-            try
-            {
-                serialPort.PortName = SelectedPort;
-                serialPort.Open();
+                    ComboBoxColor = Brushes.Gray;
+                    OutputInfo = $"{SelectedPort} is connected";
+                    OutputInfoColor = Brushes.Black;
+                    ConnectButtonName = "Disconnect";
+                    ConnectButtonColor = Brushes.Orange;
+
+                    UpdateButtonIsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    OutputInfo = ex.Message;
+                    OutputInfoColor = Brushes.Red;
+                }
             }
-            catch (Exception)
+            else if (ConnectButtonName == "Disconnect")
             {
-                MessageBox.Show("Порт занят");
+                SerialPort.Close();
+                ConnectButtonName = "Connect";
+                ConnectButtonColor = Brushes.YellowGreen;
+
+                ComboBoxColor = Brushes.Black;
+                PortIsEnabled = true;
+                OutputInfoColor = Brushes.Black;
+                OutputInfo = $"{SelectedPort} is disconnect";
+
+                UpdateButtonIsEnabled = true;
             }
+
         }
 
-        [RelayCommand]
+        [RelayCommand(CanExecute = nameof(UpdateButtonIsEnabled))]
         private void UpdatePorts()
         {
             PortNamesList = SerialPort.GetPortNames();
@@ -52,6 +99,7 @@ namespace RemoteControlArduino.ViewModels
             if (PortNamesList.Length != 0)
             {
                 SelectedPort = PortNamesList[0];
+                ConectionButtonIsEnabled = true;
             }
         }
 
